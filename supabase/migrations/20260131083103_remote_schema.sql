5a601ed0863235a1f386e8b1084e4487fa2a234d
Rahmah Putri Azzahra <93776934+rahmaptr@users.noreply.github.com>
2026-01-31 08:32:56 +0000

    chore: alter table

---
+++ supabase/migrations/20260131083103_remote_schema.sql
@@ -0,0 +1,35 @@
+drop extension if exists "pg_net";
+
+alter table "public"."profiles" add column "password" text;
+
+CREATE TRIGGER on_auth_user_created AFTER INSERT ON auth.users FOR EACH ROW EXECUTE FUNCTION public.handle_new_user();
+
+
+  create policy "Anyone can upload an avatar."
+  on "storage"."objects"
+  as permissive
+  for insert
+  to public
+with check ((bucket_id = 'avatars'::text));
+
+
+
+  create policy "Avatar images are publicly accessible."
+  on "storage"."objects"
+  as permissive
+  for select
+  to public
+using ((bucket_id = 'avatars'::text));
+
+
+CREATE TRIGGER objects_delete_delete_prefix AFTER DELETE ON storage.objects FOR EACH ROW EXECUTE FUNCTION storage.delete_prefix_hierarchy_trigger();
+
+CREATE TRIGGER objects_insert_create_prefix BEFORE INSERT ON storage.objects FOR EACH ROW EXECUTE FUNCTION storage.objects_insert_prefix_trigger();
+
+CREATE TRIGGER objects_update_create_prefix BEFORE UPDATE ON storage.objects FOR EACH ROW WHEN (((new.name <> old.name) OR (new.bucket_id <> old.bucket_id))) EXECUTE FUNCTION storage.objects_update_prefix_trigger();
+
+CREATE TRIGGER prefixes_create_hierarchy BEFORE INSERT ON storage.prefixes FOR EACH ROW WHEN ((pg_trigger_depth() < 1)) EXECUTE FUNCTION storage.prefixes_insert_trigger();
+
+CREATE TRIGGER prefixes_delete_hierarchy AFTER DELETE ON storage.prefixes FOR EACH ROW EXECUTE FUNCTION storage.delete_prefix_hierarchy_trigger();
+
+
